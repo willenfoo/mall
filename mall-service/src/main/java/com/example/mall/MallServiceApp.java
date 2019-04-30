c721b54e9f6891bc95ac7985a8ac782806623016
willenfoo <willenfoo@foxmail.com>
2019-04-30 16:55:44 +0800

    11

--- mall-service/src/main/java/com/example/mall/MallServiceApp.java
+++ mall-service/src/main/java/com/example/mall/MallServiceApp.java
@@ -1,14 +1,24 @@
 package com.example.mall;
 
+import com.baomidou.mybatisplus.dts.EnableDtsRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDtsRabbit
 @EnableEurekaClient
 @EnableScheduling
 public class MallServiceApp {
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MallServiceApp.class, args);
